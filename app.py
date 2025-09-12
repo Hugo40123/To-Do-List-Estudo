4077d1ce515ca70c82539441f119fcd15a13c66f
Hugo Lopes <hugo40123@gmail.com>
2025-09-11 22:12:52 -0300

    Commit inicial To-Do-List com Flask

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+tarefas = ["Comprar leite", "Estudar Python", "Limpar quarto"]
+
+@app.route("/")
+def home():
+    return render_template("index.html", tarefas=tarefas)
+
+from flask import request, redirect, url_for
+
+@app.route("/add", methods=["POST"])
+def add():
+    tarefa = request.form.get("tarefa")
+    if tarefa:
+        tarefas.append(tarefa)
+    return redirect(url_for("home"))
+
+@app.route("/remove", methods=["POST"])
+def remove():
+    tarefa = request.form.get("tarefa")
+    if tarefa in tarefas:
+        tarefas.remove(tarefa)
+    return redirect(url_for("home"))
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+
