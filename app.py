c230864a7e074a5ea53554f9e3cf2f270cf02a5f
Hugo Lopes <hugo40123@gmail.com>
2025-09-13 15:19:47 -0300

    Adicionado a função de categoria

--- app.py
+++ app.py
@@ -13,12 +13,20 @@ def home():
     return render_template("index.html", tarefas=tarefas)
 
 from flask import request, redirect, url_for
+from datetime import datetime
 
 @app.route("/add", methods=["POST"])
 def add():
     tarefa = request.form.get("tarefa")
+    prazo_str = request.form.get("dataHora")
+    prazo_formatado = None
+    categoria = request.form.get("categoria")
+
+    if prazo_str:
+        prazo = datetime.fromisoformat(prazo_str)
+        prazo_formatado = prazo.strftime("%d/%m/%Y %H:%M")
     if tarefa:
-        tarefas.append({"texto":tarefa, "feito": False, "prazo": prazo})
+        tarefas.append({"texto":tarefa, "feito": False, "prazo": prazo_formatado, "categoria": categoria})
     return redirect(url_for("home"))
 
 @app.route("/concluir", methods=["POST"])
